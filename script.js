ddad49d3d6e0552cb9c7a64c9e64f3b91b8ec3f2
Tarun-vv <167981890+Tarun-vv@users.noreply.github.com>
2024-06-18 08:47:23 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+// FOR THE ROADSIDE IMAGES
+
+const btn = document.querySelector('.btn');
+[...document.querySelectorAll('.btn')].forEach(function(i) {
+  btn[i].classList.toggle('.fill')
+})
+
+document.querySelector(".btn1").addEventListener("click", function () {
+  console.log("hello");
+  document.querySelector(".btn1").classList.toggle("fill");
+});
+
+document.querySelector(".btn2").addEventListener("click", function () {
+  console.log("hello");
+  document.querySelector(".btn2").classList.toggle("fill");
+});
+
+document.querySelector(".btn3").addEventListener("click", function () {
+  console.log("hello");
+  document.querySelector(".btn3").classList.toggle("fill");
+});
+
+document.querySelector(".btn4").addEventListener("click", function () {
+  console.log("hello");
+  document.querySelector(".btn4").classList.toggle("fill");
+});
+
+document.querySelector(".btn5").addEventListener("click", function () {
+  console.log("hello");
+  document.querySelector(".btn5").classList.toggle("fill");
+});
+
+document.querySelector(".btn6").addEventListener("click", function () {
+  console.log("hello");
+  document.querySelector(".btn6").classList.toggle("fill");
+});
+
+// NATURE SHOTS : 
+
+document.querySelector(".nature-btn1").addEventListener("click", function () {
+  console.log("hello");
+  document.querySelector(".nature-btn1").classList.toggle("fill");
+});
+
+document.querySelector(".nature-btn2").addEventListener("click", function () {
+  console.log("hello");
+  document.querySelector(".nature-btn2").classList.toggle("fill");
+});
+
+document.querySelector(".nature-btn3").addEventListener("click", function () {
+  console.log("hello");
+  document.querySelector(".nature-btn3").classList.toggle("fill");
+});
+
+document.querySelector(".nature-btn4").addEventListener("click", function () {
+  console.log("hello");
+  document.querySelector(".nature-btn4").classList.toggle("fill");
+});
+
+document.querySelector(".nature-btn5").addEventListener("click", function () {
+  console.log("hello");
+  document.querySelector(".nature-btn5").classList.toggle("fill");
+});
+
+document.querySelector(".nature-btn6").addEventListener("click", function () {
+  console.log("hello");
+  document.querySelector(".nature-btn6").classList.toggle("fill");
+});
