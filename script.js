8d9e02b1bb764c4a9dc059461b2bf723a0ea7b27
Tarun-vv <167981890+Tarun-vv@users.noreply.github.com>
2024-07-30 13:59:54 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -13,14 +13,17 @@ let src = "";
 const img = document.querySelectorAll(".img");
 const changingImg = document.querySelector(".changing-img");
 const overlay = document.querySelector(".overlay");
-const imgZoom = document.querySelector(".img-zoom-box");
 const closeBtn = document.querySelector(".close-btn");
+const increaseSizeBtn = document.querySelector(".increase-size-btn");
+const decreaseSizeBtn = document.querySelector(".decrease-size-btn");
 const body = document.querySelector("body");
 console.log(img);
 for (let i = 0; i < img.length; i++) {
   img[i].addEventListener("click", function () {
     overlay.classList.remove("hidden");
     closeBtn.classList.remove("hidden");
+    increaseSizeBtn.classList.remove("hidden");
+    decreaseSizeBtn.classList.remove("hidden");
     changingImg.classList.remove("hidden");
     src = img[i].src;
     changingImg.src = src;
@@ -31,15 +34,27 @@ closeBtn.addEventListener("click", function () {
   overlay.classList.add("hidden");
   changingImg.classList.add("hidden");
   closeBtn.classList.add("hidden");
+  increaseSizeBtn.classList.add("hidden");
+  decreaseSizeBtn.classList.add("hidden");
+  changingImg.style.width = "50%";
 });
 
 overlay.addEventListener("click", function () {
   overlay.classList.add("hidden");
   changingImg.classList.add("hidden");
   closeBtn.classList.add("hidden");
+  increaseSizeBtn.classList.add("hidden");
+  decreaseSizeBtn.classList.add("hidden");
+  changingImg.style.width = "50%";
 });
 
-// Comment Feature
+increaseSizeBtn.addEventListener("click", function () {
+  changingImg.style.width = "58%";
+});
+
+decreaseSizeBtn.addEventListener("click", function () {
+  changingImg.style.width = "45%";
+}); // Comment Feature
 
 const commentBtn = document.querySelector(".btn-comment");
 const commentSection = document.querySelector(".comment-section");
@@ -49,3 +64,7 @@ commentBtn.addEventListener("click", function () {
 });
 
 console.log("hello");
+
+const obj1 = { a: 1, b: 2 };
+const obj2 = { c: 3, d: 4 };
+
